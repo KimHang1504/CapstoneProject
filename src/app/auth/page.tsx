94e23c5f1fce71f0dd39bb800af72e1343ec6bee
LeHuyVuu <lehuyvuok@gmail.com>
2026-03-25 22:26:13 +0700

    feat(auth): restrict Google login for MEMBER accounts

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -62,13 +62,18 @@ export default function LoginPage() {
         try {
             const res = await loginWithGoogle({ idToken });
             const token = res.data.accessToken;
-            apiClient.setAuthToken(token);
-
-            document.cookie = `accessToken=${res.data.accessToken}; path=/; max-age=36000;Secure`;
-            window.dispatchEvent(new Event('login'));
 
             const user = getUserFromToken(token);
             const role = user.role;
+            if (role === "MEMBER") {
+                toast.error("Tài khoản MEMBER không được phép đăng nhập bằng Google.");
+                return;
+            }
+
+            apiClient.setAuthToken(token);
+            document.cookie = `accessToken=${res.data.accessToken}; path=/; max-age=36000;Secure`;
+            window.dispatchEvent(new Event('login'));
+
             if (role === "ADMIN") {
                 nav.push("/admin");
             }
